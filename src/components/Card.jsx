9f7eb58b05048d940a018387841c6b93737ce9a0
HaojieZhang123 <haojiezhang123@gmail.com>
2025-05-08 15:58:33 +0200

    [FEAT] added states on all elements with logic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,9 @@
 const Card = (props) => {
     return (
         <>
-            <div className="card">
+            {/* If isActive is true, the card class becomes card-active class */}
+            {/* default card stay hidden, card-active will be displayed */}
+            <div className={"card" + (props.isActive ? '-active' : '')}>
                 <h2>{props.obj.title}</h2>
                 <div className="description">
                     {props.obj.description}
